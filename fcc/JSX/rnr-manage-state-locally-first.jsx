611d4a88f621e589cb00f6284885de696763f970
Isaac Sokari <isaacsokari@gmail.com>
2020-06-15 17:13:34 +0100

    finished react and redux challenges

---
+++ fcc/JSX/rnr-manage-state-locally-first.jsx
@@ -0,0 +1,50 @@
+/* React and Redux: Manage State Locally First
+Here you'll finish creating the DisplayMessages component.
+
+First, in the render() method, have the component render an input element, button element, and ul element. When the input element changes, it should trigger a handleChange() method. Also, the input element should render the value of input that's in the component's state. The button element should trigger a submitMessage() method when it's clicked.
+
+Second, write these two methods. The handleChange() method should update the input with what the user is typing. The submitMessage() method should concatenate the current message (stored in input) to the messages array in local state, and clear the value of the input.
+
+Finally, use the ul to map over the array of messages and render it to the screen as a list of li elements.
+*/
+
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+  submitMessage() {
+    const currentMessage = this.state.input;
+    this.setState({
+      input: '',
+      messages: this.state.messages
+        .concat(currentMessage)
+        .filter((one) => one.trim() !== ''),
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input value={this.state.input} onChange={this.handleChange} />
+        <br />
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.state.messages.map((message, idx) => {
+            return <li key={idx}>{message}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
